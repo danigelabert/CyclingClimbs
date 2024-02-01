c1a563312ca27e2b8b68b51f277da30b43e09420
danigelabert <73782173+danigelabert@users.noreply.github.com>
2024-02-01 09:13:18 +0100

    Compile 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.danigelabert.cyclingclimbs"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.danigelabert.cyclingclimbs"
